2d700f4a2abd8465134c4cfa3217c58207582a89
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-08-22 16:51:59 +0500

    OS module

--- app.js
+++ app.js
@@ -16,7 +16,17 @@
 // const logger = require("./logger");
 // logger.log("this is a message");
 
-const path = require("path");
-var pathObj = path.parse(__filename);
+// const path = require("path");
+// var pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+const os = require("os");
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+var type = os.type();
+var uptime = os.uptime();
+console.log(
+  `${freeMemory} is available from ${totalMemory} of OStype ${type} has Up time of ${uptime}`
+);
