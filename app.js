b428a44f2aac1c0f4a111bcc51b0c9c9b10f740b
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-08-22 14:33:59 +0500

    global object replace window in node

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// globel scope are accessed in browser by window object e.g window.conole.log()  , window.setTimout()
+// but we dont have window object in node so they are accessed by
+// global e.g global.console.log()
+
+var name = "Javed";
+// console.log(window.name);  exist in browser
+console.log(global.name); // undefined in global
 function hello(name) {
-  console.log(`hello ${name}`);
+  global.console.log(`hello ${name}`); // console is global scope
 }
 
 hello("javed");
